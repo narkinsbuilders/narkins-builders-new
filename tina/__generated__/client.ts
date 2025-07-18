d925b73968aba061a339d39198c08247fa3436a9
imossaidqadri <imossaidquadri@gmail.com>
2025-07-19 01:45:52 +0500

    fix: Generate TinaCMS schema files and resolve build errors
    - Remove TinaCMSProvider from _app.tsx to fix static generation
    - Create fallback admin interface with clear setup instructions
    - Generate proper TinaCMS schema files with build command
    - Fix all TypeScript errors and build issues

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ cacheDir: '/home/ossaid/work/narkins-builders/tina/__generated__/.cache/1752871505903', url: 'http://localhost:4001/graphql', token: 'test', queries,  });
 export default client;
   
