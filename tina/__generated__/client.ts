e44ce818b0d7993a1d2a043aa9d26421cf2fc1bb
imossaidqadri <imossaidquadri@gmail.com>
2025-07-31 15:30:11 +0500

    dependencies security updates

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '53e7f313a01bc4bebc89cfd2ca0033b8413875f7', queries,  });
 export default client;
   
