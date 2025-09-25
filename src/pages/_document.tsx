5d63d7dcc05a90896ecff06ccb04d9b97c053cf2
imossaidqadri <imossaidquadri@gmail.com>
2025-09-25 16:38:03 +0500

    updated pixel id

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,8 @@ export default function Document() {
        t.src=v;s=b.getElementsByTagName(e)[0];
        s.parentNode.insertBefore(t,s)}(window, document,'script',
        'https://connect.facebook.net/en_US/fbevents.js');
-       fbq('init', '5304750006256624');
+       fbq('init', '809121658472233');
+       fbq('track', 'PageView');
       `,
      }}
     />
@@ -47,7 +48,7 @@ export default function Document() {
       height="1" 
       width="1" 
       style={{display:'none'}}
-      src="https://www.facebook.com/tr?id=5304750006256624&ev=PageView&noscript=1"
+      src="https://www.facebook.com/tr?id=809121658472233&ev=PageView&noscript=1"
       alt=""
      />
     </noscript>
